3b02df3520042f78896e66275f450d5d9bdb5a72
KiranKrishna004 <kiran.004narath@gmail.com>
2022-02-08 17:35:36 +0530

    Modified Videobanner

--- components/VideoBanner.js
+++ components/VideoBanner.js
@@ -1,44 +1,20 @@
 import React from 'react'
-import Button from './Button'
+
 const VideoBanner = () => {
-  const image = [
-    {
-      image: 'https://www.wallpaperkiss.com/wimg/b/217-2178784_big.jpg',
-    },
-  ]
   return (
-    <div>
-      {' '}
-      <div
-        className="relative mt-5"
-        style={{ maxWidth: 'none', maxHeight: 'none', height: 'none' }}
-      >
-        {image.map((slide, index) => {
-          return (
-            <img
-              src={slide.image}
-              alt="This is a carousel slide"
-              key={index}
-              style={{}}
-              className="overflow-hidden"
-            />
-          )
-        })}
-        <div
-          style={{
-            width: '100%',
-            // height: '4px',
-            position: 'absolute',
-            bottom: '50%',
-            textAlign: '-webkit-center',
-          }}
-        >
-          <Button btnColor="#16a34a" type="outline">
-            Recycled / Handmade
-          </Button>
-        </div>
-      </div>
+    <div className="mt-7 flex w-5/6 overflow-hidden border-solid hover:border-double xs:h-56 xs:w-full sm:h-96 sm:border-4 sm:p-2 xl:w-1/2">
+      <iframe
+        className="h-full w-full justify-center overflow-hidden"
+        width="60%"
+        height="80%"
+        src="https://www.youtube.com/embed/ScMzIvxBSi4"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
     </div>
   )
 }
+{/* <iframe width="717" height="403" src="https://www.youtube.com/embed/ScMzIvxBSi4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
 export default VideoBanner
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,11 +14,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Main />
-      <div className="flex h-full w-full flex-wrap justify-around">
-        <Shopcard imgLink={true} redirectLink={true} />
-        <Shopcard imgLink={true} redirectLink={true} />
-        <Shopcard imgLink={true} redirectLink={true} />
-      </div>
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
@@ -27,17 +22,14 @@ export default function Home() {
           </a>
         </h1>
       </main>
-      {/* <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="ml-2 h-4" />
-        </a>
-      </footer> */}
+      <div className="h-full w-full flex-wrap justify-around xs:hidden md:flex">
+        <Shopcard imgLink={true} redirectLink={true} />
+        <Shopcard imgLink={true} redirectLink={true} />
+        <Shopcard imgLink={true} redirectLink={true} />
+      </div>
+      <div className="flex h-full w-full flex-wrap justify-around md:hidden">
+        <Shopcard imgLink={true} redirectLink={true} />
+      </div>
       <VideoBanner />
       <Footer />
     </div>
