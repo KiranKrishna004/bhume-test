4ce0fde51135b947eaaffff2943664b7f35bb2a2
nashvincent <36297311+nashvincent@users.noreply.github.com>
2022-02-19 00:56:07 +0530

    Removed placeholder some text

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
         <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
           <h1 className="text-6xl font-bold">
             Welcome to{' '}
-            <a className="text-green-600" href="https://nextjs.org">
+            <a className="text-green-600" href="https://bhume.co.in/">
               BhuME
             </a>
           </h1>
