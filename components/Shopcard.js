14615865e4e04ce10ce0c5b94ae6825788da26f7
Unniettan12 <harikrishnanrk00@gmail.com>
2022-02-18 17:59:07 +0530

    Added effect on shop card

--- components/Shopcard.js
+++ components/Shopcard.js
@@ -4,8 +4,11 @@ import ShopButton from './ShopButton'
 const Shopcard = ({ imgLink, redirectLink }) => {
   return (
     // <div className="flex h-full w-full flex-wrap justify-around">
-    <div className="relative h-auto flex-1">
-      <img src={imgLink} />
+    <div className="relative h-auto flex-1 overflow-hidden bg-black">
+      <img
+        src={imgLink}
+        className="h-full w-full transform bg-cover bg-center transition-all duration-500 ease-in-out hover:scale-150 hover:opacity-50"
+      />
       <div
         style={{
           position: 'absolute',
